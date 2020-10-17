67b2e159602fcee05b111bf25f29857745debb5a
Ashkan Jalali <55811147+ashkanjalaliQ@users.noreply.github.com>
2020-10-17 14:20:23 +0330

    fix error

--- pardazesh.py
+++ pardazesh.py
@@ -13,23 +13,27 @@ def find_max(ls):
 
 def check_command():
     user_exit = True
+    error = False
     while user_exit:
         entry = input('Please Enter Command').replace('"', '')
         if '-r' in entry:
             if len(re.findall('(-r|-nr) [A-Z:a-z_]*.(png|jpg|jpeg|gif) edit (grayscale|negative|blackandwhite|contour|edgeenhance|emboss|findedges|blur|smooth)* [a-z]*', entry)) != 0:
                 user_exit = False
-            if len(re.findall('-r [A-Za-z1-9.]*.(png|jpg|jpeg|gif) tool image_to_text [a-z]*', entry)) != 0:
+            elif len(re.findall('-r [A-Za-z1-9.]*.(png|jpg|jpeg|gif) tool image_to_text [a-z]*', entry)) != 0:
                 user_exit = False
+            if user_exit == True:
+                error = True
         elif '-nr' in entry:
             if '/' in (entry.split()[1]).split('.')[0] or ('_' in (entry.split()[1]).split('.')[0] or '-' in (entry.split()[1]).split('.')[0] or '(' in (entry.split()[1]).split('.')[0] or ')' in (entry.split()[1]).split('.')[0]):
                 if len(re.findall('-nr [^,;]+.(png|jpg|jpeg|gif) edit (grayscale|negative|blackandwhite|contour|edgeenhance|emboss|findedges|blur|smooth)* [a-z]*',entry)) != 0:
                     user_exit = False
                 elif len(re.findall('-nr [^,;]+.(png|jpg|jpeg|gif) tool image_to_text (fas|eng) [a-z]*', entry)) != 0:
                     user_exit = False
+                if user_exit == True:
+                    error = True
         elif entry == '--help':
-            #if entry == '--help':
             print(settings.HELP)
-        else:
+        if error == True:
             print(settings.ERROR[0])
     return user_exit, Processing_sentence(entry)
 
