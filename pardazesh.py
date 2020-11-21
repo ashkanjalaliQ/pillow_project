3f64b0699da6b5d15f389d013470aa88d57507d8
Ashkan Jalali <55811147+ashkanjalaliQ@users.noreply.github.com>
2020-11-21 13:36:53 +0330

    Update pardazesh.py

--- pardazesh.py
+++ pardazesh.py
@@ -6,3 +6,23 @@ def check_command():
     entry = input('Please Enter Command').replace('"', '')
     user_exit = False
     return user_exit, entry.split()
+
+
+def find_max(ls):
+    _max_ = max(ls)
+    for i in range(len(ls)):
+        if ls[i] == _max_:
+            return i
+
+
+
+def recommender(word, options):
+    p = []
+    word = word.strip()
+    for option in options:
+        t = 0
+        for letter in option.strip():
+            if letter in word:
+                t += 1
+        p.append(t / len(option))
+    return options[find_max(p)]
